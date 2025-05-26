0ed9dd0b39d54bbff10e25fb1ebcb711912502be
Zhihang Shao <dio_ro@outlook.com>
2025-05-26 13:27:43 +0800

    fix: change sys_kill return value for busybox kill testcase

--- api/src/imp/signal.rs
+++ api/src/imp/signal.rs
@@ -104,33 +104,40 @@ pub fn sys_kill(pid: i32, signo: u32) -> LinuxResult<isize> {
         return Ok(0);
     };
 
+
+    let mut result = 0usize;
     let curr = current();
     match pid {
         1.. => {
             let proc = get_process(pid as Pid)?;
             send_signal_process(&proc, sig)?;
-            Ok(1)
+            result += 1;
         }
         0 => {
             let pg = curr.task_ext().thread.process().group();
-            Ok(send_signal_process_group(&pg, sig) as _)
+            result += send_signal_process_group(&pg, sig);
         }
         -1 => {
-            let mut count = 0;
             for proc in processes() {
                 if proc.is_init() {
                     // init process
                     continue;
                 }
                 send_signal_process(&proc, sig.clone())?;
-                count += 1;
+                result += 1;
             }
-            Ok(count)
         }
         ..-1 => {
             let pg = get_process_group((-pid) as Pid)?;
-            Ok(send_signal_process_group(&pg, sig) as _)
+            result += send_signal_process_group(&pg, sig);
         }
+    }
+    info!("[sys_kill] successfully sent signal {} processes", result);
+
+    if result > 0 {
+        Ok(0)
+    } else {
+        Err(LinuxError::ESRCH)
     }
 }
 
